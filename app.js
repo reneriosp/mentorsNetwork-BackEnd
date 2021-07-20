eb93436d12d2878e7c9549c0194797e8c9d3a64c
Robin Edmunds <8656189+robinedmunds@users.noreply.github.com>
2021-07-20 15:07:30 +0100

    BUGGED: Add express-openid-connect boilerplate to app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express")
 const path = require("path")
 const cookieParser = require("cookie-parser")
 const logger = require("morgan")
+const { auth } = require("express-openid-connect")
+const auth0Config = require("./auth0/config")
 
 const indexRouter = require("./routes/index")
 const usersRouter = require("./routes/users")
@@ -15,6 +17,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
+app.use(auth(auth0Config))
 app.use("/", indexRouter)
 app.use("/users", usersRouter)
 
