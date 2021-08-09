407ab3672f3929a2d04fa6efb63e5822b789b87c
karmanya <karmanya@pop-os.localdomain>
2021-08-09 10:33:29 +0530

    adding db interaction scripts

---
+++ dbinteraction.js
@@ -0,0 +1,40 @@
+class DBInteraction {
+    constructor() {
+        var AWS = require("aws-sdk");
+        var credentials = new AWS.SharedIniFileCredentials({ profile: "ethanevans" });
+        AWS.config.credentials = credentials;
+        console.log(AWS.config.credentials.accessKeyId);
+        AWS.config.update({ region: "us-east-1" });
+        this.dynamoDB = new AWS.DynamoDB();
+        this.docClient = new AWS.DynamoDB.DocumentClient();
+    }
+    writeRecord(tableName, record) {
+        let obj = {
+            TableName: tableName,
+            Item: record,
+        };
+        this.docClient.put(obj, function (err, data) {
+            if (err) console.error("Unable to add item. Error", JSON.stringify(err, null, 2));
+            else console.log("Added item", JSON.stringify(data, null, 2));
+        });
+    }
+    readRecord(tableName, keys, callback) {
+        let obj = {
+            TableName: tableName,
+            Key: keys,
+        };
+        this.docClient.get(obj, function (err, data) {
+            if (err) console.error("Unable to read item", JSON.stringify(err, null, 2));
+            else {
+                console.log("Got Item", JSON.stringify(data, null, 2));
+                if (callback && typeof callback === "function") callback(data);
+            }
+        });
+    }
+    updateRecord(tableName, keys, updateExpression, updateAttributes) {}
+}
+
+interaction = new DBInteraction();
+interaction.writeRecord("Movies", { year: 2016, title: "Fancy Pants Movie2", info: { plot: "Blah", rating: 0 } });
+interaction.readRecord("Movies", { year: 2015, title: "Fancy Pants Movie" });
+interaction.updateRecord;
